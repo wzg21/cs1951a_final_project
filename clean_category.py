af2ba9b1b415630be6f6c672a9f0c6a7db836a96
jydong <jingyan4798@gmail.com>
2020-04-15 13:21:14 -0400

    added clean_categories() to clean_category.py; generated businesses_nyc_small.csv with fewer features and indicator variables for category

--- clean_category.py
+++ clean_category.py
@@ -4,23 +4,65 @@ import pandas as pd
 
 b_file_path = "./businesses_nyc.csv"
 c_file_path = "./categories_nyc.txt"
-
-d = defaultdict(int)
-
+b_category_file_path = "./businesses_nyc_small.csv"
 df = pd.read_csv(b_file_path)
-categories = df['category_alias']
-for index, row in df.iterrows():
-    # print(row['category_alias'])
-    categories = row['category_alias'].split(",")
-    for c in categories:
-    	d[c] += 1
+
+def count_categories():
+	d = defaultdict(int)
+
+	# df = pd.read_csv(b_file_path)
+	categories = df['category_alias']
+	for index, row in df.iterrows():
+	    # print(row['category_alias'])
+	    categories = row['category_alias'].split(",")
+	    for c in categories:
+	    	d[c] += 1
 
 
-c_count = open(c_file_path,"a") 
- 
-for w in sorted(d, key=d.get, reverse=True):
-    print(w, d[w])
-    c_count.write(w + "," + str(d[w]) + "\n")
+	c_count = open(c_file_path,"a") 
+	 
+	for w in sorted(d, key=d.get, reverse=True):
+	    print(w, d[w])
+	    c_count.write(w + "," + str(d[w]) + "\n")
 
-print(len(d))
-c_count.close()
+	print("number of categories: " + str(len(d)))
+	c_count.close()
+
+
+
+
+popular_categories = ["italian", "breakfast_brunch"]
+
+# generate a smaller businesses dataset with popular categories as indicator variables 
+def clean_categories():
+	b_keys = ["id", "name", "review_count", "category_alias", "rating", "latitude", "longitude", "delivery", "pickup",
+		"restaurant_reservation", "price", "city", "zip_code", "country", "state", "distance"] + popular_categories
+
+	# write headers to csv files
+	with open(b_category_file_path, 'w', newline='') as b_csv:
+	    writer = csv.writer(b_csv)
+	    writer.writerow(b_keys)
+
+	for index, row in df.iterrows():
+		row_small = row[['id', 'name', 'review_count', 'category_alias', 'rating', 'latitude', 'longitude', 'delivery', 
+	    	'pickup', 'restaurant_reservation', 'price', 'city', 'zip_code', 'country', 'state', 'distance']]
+		row_small_list = row_small.values.tolist()
+
+		cat = [0 for i in range(len(popular_categories))]
+		categories = row['category_alias'].split(",")
+		for c in categories:
+			if c in popular_categories:
+				cat[popular_categories.index(c)] = 1
+
+		new_row = row_small_list + cat
+		write_to_csv(b_category_file_path, new_row)
+
+def write_to_csv(file, row):
+    with open(file, 'a') as csvfile:
+      writer = csv.writer(csvfile)
+      writer.writerow(row)
+
+
+# count_categories()
+clean_categories()
+
